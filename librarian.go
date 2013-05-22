406a5bd7cb076d0be91a456d9cc2fc6dd1503328
Chuck Preslar <chuckpreslar@gmail.com>
2013-05-21 20:55:13 -0400

    Join for []columns.

--- librarian.go
+++ librarian.go
@@ -1,9 +1,21 @@
 package librarian
 
+import (
+	"strings"
+)
+
 type column string
 type columns []column
 type table string
 type accessor func(string) column
+
+func (c columns) join(d string) string {
+	s := make([]string, len(c))
+	for _, cc := range c {
+		s = append(s, string(cc))
+	}
+	return strings.Join(s, d)
+}
 
 func Search(a accessor) *SelectStatement {
 	return &SelectStatement{a: a, projections: []column{}, reference: table(a(""))}
