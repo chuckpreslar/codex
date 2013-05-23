b5bfe31ae0b25a11d9ef43dc386a9c1dfe672299
Chuck Preslar <chuckpreslar@gmail.com>
2013-05-23 14:49:25 -0400

    Use ticks to avoid tricky string escapes.

--- types.go
+++ types.go
@@ -41,56 +41,56 @@ type results []result
 func Table(t string) accessor {
 	return func(c string) column {
 		switch c {
-		case "":
-			return column(fmt.Sprintf("\"%s\"", t))
-		case "*":
-			return column(fmt.Sprintf("\"%s\".%s", t, c))
+		case ``:
+			return column(fmt.Sprintf(`"%s"`, t))
+		case `*`:
+			return column(fmt.Sprintf(`"%s".%s`, t, c))
 		default:
-			return column(fmt.Sprintf("\"%s\".\"%s\"", t, c))
+			return column(fmt.Sprintf(`"%s"."%s"`, t, c))
 		}
 	}
 }
 
 func (a accessor) On(s string) column {
-	return column(fmt.Sprintf("%s ON %s", a(""), a(s)))
+	return column(fmt.Sprintf(`%s ON %s`, a(``), a(s)))
 }
 
 func (c column) Eq(v interface{}) expression {
 	if v == nil {
-		return expression(fmt.Sprintf("%s IS NULL", c))
+		return expression(fmt.Sprintf(`%s IS NULL`, c))
 	}
-	return expression(fmt.Sprintf("%s = %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s = %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) Neq(v interface{}) expression {
 	if v == nil {
-		return expression(fmt.Sprintf("%s IS NOT NULL", c))
+		return expression(fmt.Sprintf(`%s IS NOT NULL`, c))
 	}
-	return expression(fmt.Sprintf("%s != %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s != %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) Gt(v interface{}) expression {
-	return expression(fmt.Sprintf("%s > %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s > %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) Gte(v interface{}) expression {
-	return expression(fmt.Sprintf("%s >= %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s >= %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) Lt(v interface{}) expression {
-	return expression(fmt.Sprintf("%s < %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s < %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) Lte(v interface{}) expression {
-	return expression(fmt.Sprintf("%s <= %s", c, formatExpressionValue(v)))
+	return expression(fmt.Sprintf(`%s <= %s`, c, formatExpressionValue(v)))
 }
 
 func (c column) As(s string) column {
-	return column(fmt.Sprintf("%s AS \"%s\"", c, s))
+	return column(fmt.Sprintf(`%s AS "%s"`, c, s))
 }
 
 func (e expression) Or(ee expression) expression {
-	return expression(fmt.Sprintf("%s OR %s", e, ee))
+	return expression(fmt.Sprintf(`%s OR %s`, e, ee))
 }
 
 /**
