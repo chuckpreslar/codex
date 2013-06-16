281643e3c406a25594c529246625b13975b30bcf
Chuck Preslar <chuckpreslar@gmail.com>
2013-06-16 16:37:07 -0400

    Improve VisitorInterface.

--- visitors/interfaces.go
+++ visitors/interfaces.go
@@ -7,4 +7,17 @@ import (
 type VisitorInterface interface {
   Accept(nodes.NodeInterface) string
   visit(interface{}) string
+  visitEqNode(*nodes.EqNode) string
+  visitNeqNode(*nodes.NeqNode) string
+  visitGtNode(*nodes.GtNode) string
+  visitGteNode(*nodes.GteNode) string
+  visitLtNode(*nodes.LtNode) string
+  visitLteNode(*nodes.LteNode) string
+  visitOrNode(*nodes.OrNode) string
+  visitSqlFunctionNode(*nodes.SqlFunctionNode) string
+  visitAttributeNode(*nodes.AttributeNode) string
+  visitString(string) string
+  visitInt(int) string
+  visitFloat64(float64) string
+  functionName(string) string
 }
