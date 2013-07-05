4ae7fd3cc5dcb3c2b46df9f2df31c212e0d12c6a
Chuck Preslar <chuckpreslar@gmail.com>
2013-07-05 10:45:52 -0400

    Better NotEqual test.

--- test/members/nodes/not_equal_test.go
+++ test/members/nodes/not_equal_test.go
@@ -7,10 +7,34 @@ import (
 
 func TestNotEqual(t *testing.T) {
   left, right := 1, 2
-  equal := &nodes.NotEqual{&nodes.Binary{left, right}}
-  if left != equal.Left {
-    t.Errorf("Expect Left NotEqual leaf to equal %v, got %v.", left, equal.Left)
-  } else if right != equal.Right {
-    t.Errorf("Expect Right NotEqual leaf to equal %v, got %v.", right, equal.Right)
+  notEqual := &nodes.NotEqual{&nodes.Binary{left, right}}
+  if left != notEqual.Left {
+    t.Errorf("Expect Left NotEqual leaf to equal %v, got %v.", left, notEqual.Left)
+  } else if right != notEqual.Right {
+    t.Errorf("Expect Right NotEqual leaf to equal %v, got %v.", right, notEqual.Right)
+  }
+}
+
+func TestNotEqualOr(t *testing.T) {
+  left, right := 1, 2
+  notEqual := &nodes.NotEqual{&nodes.Binary{left, right}}
+  other := 3
+  or := notEqual.Or(other)
+  if notEqual != or.Left {
+    t.Errorf("Expect Left Or leaf to equal %v, got %v.", notEqual, notEqual.Left)
+  } else if other != or.Right {
+    t.Errorf("Expect Right Or leaf to equal %v, got %v.", other, notEqual.Right)
+  }
+}
+
+func TestNotEqualAnd(t *testing.T) {
+  left, right := 1, 2
+  notEqual := &nodes.NotEqual{&nodes.Binary{left, right}}
+  other := 3
+  and := notEqual.And(other)
+  if notEqual != and.Left {
+    t.Errorf("Expect Left And leaf to equal %v, got %v.", notEqual, notEqual.Left)
+  } else if other != and.Right {
+    t.Errorf("Expect Right And leaf to equal %v, got %v.", other, notEqual.Right)
   }
 }
